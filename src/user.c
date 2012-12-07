9982e50b891074a711c43c5d7acc71cb793eb14d
Matthias Clasen <mclasen@redhat.com>
2012-12-06 23:24:02 -0500

    Add missing notification

--- src/user.c
+++ src/user.c
@@ -308,36 +308,42 @@ user_update_from_keyfile (User     *user,
                 /* TODO: validate / normalize */
                 g_free (user->language);
                 user->language = s;
+                g_object_notify (G_OBJECT (user), "language");
         }
 
         s = g_key_file_get_string (keyfile, "User", "XSession", NULL);
         if (s != NULL) {
                 g_free (user->x_session);
                 user->x_session = s;
+                g_object_notify (G_OBJECT (user), "x-session");
         }
 
         s = g_key_file_get_string (keyfile, "User", "Email", NULL);
         if (s != NULL) {
                 g_free (user->email);
                 user->email = s;
+                g_object_notify (G_OBJECT (user), "email");
         }
 
         s = g_key_file_get_string (keyfile, "User", "Location", NULL);
         if (s != NULL) {
                 g_free (user->location);
                 user->location = s;
+                g_object_notify (G_OBJECT (user), "location");
         }
 
         s = g_key_file_get_string (keyfile, "User", "PasswordHint", NULL);
         if (s != NULL) {
                 g_free (user->password_hint);
                 user->password_hint = s;
+                g_object_notify (G_OBJECT (user), "password-hint");
         }
 
         s = g_key_file_get_string (keyfile, "User", "Icon", NULL);
         if (s != NULL) {
                 g_free (user->icon_file);
                 user->icon_file = s;
+                g_object_notify (G_OBJECT (user), "icon-file");
         }
 
         g_object_thaw_notify (G_OBJECT (user));
